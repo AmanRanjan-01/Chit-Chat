c34c2d0bd4a4023ed93ec50e8b799021a94db9ce
Aman Ranjan <aman.ranjan.529@gmail.com>
2024-07-21 22:29:14 +0530

    updating the Socket URL

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children})=>{
     const {authUser} = useAuthContext();
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://chit-chat-qqvd.onrender.com",{
                 query:{
                     userId : authUser._id
                 }
